1a4e63cf5ae1286c03288ea715bd7343862a9f47
Akanksha Mali <akankshamali233@gmail.com>
2026-04-27 14:59:25 +0530

    spread operator,destructiong array

---
+++ script.js
@@ -0,0 +1,14 @@
+let btn1 = document.querySelector("#btn1");
+
+// btn1.onclick = (evt) => {
+//     console.log(evt);
+//     console.log(evt.type);
+//     console.log(evt.target);
+//     console.log(evt.clientX,evt.clientY);
+// };
+
+btn1.addEventListener("click", () => { 
+console.log("button1 was clicked")
+})
+
+let div = document.querySelector("div");
